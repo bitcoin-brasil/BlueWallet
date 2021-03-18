7edb72aa330dae6e61915023646be85ff2aede18
Ivan Vershigora <ivan.vershigora@gmail.com>
2021-03-18 10:21:00 +0300

    ADD: SignVerify screen

--- screen/wallets/signVerify.js
+++ screen/wallets/signVerify.js
@@ -1,7 +1,18 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { ActivityIndicator, Alert, Keyboard, Platform, StyleSheet, TextInput, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
 import { useRoute, useTheme } from '@react-navigation/native';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+
 import { BlueDoneAndDismissKeyboardInputAccessory, BlueFormLabel, BlueSpacing10, BlueSpacing20, SafeBlueArea } from '../../BlueComponents';
 import navigationStyle from '../../components/navigationStyle';
 import { FContainer, FButton } from '../../components/FloatButtons';
@@ -12,16 +23,19 @@ const SignVerify = () => {
   const { colors } = useTheme();
   const { wallets, sleep } = useContext(BlueStorageContext);
   const { params } = useRoute();
-  const [isToolbarVisibleForAndroid, setIsToolbarVisibleForAndroid] = useState(false);
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
   const [address, setAddress] = useState(params.address);
   const [message, setMessage] = useState('');
   const [signature, setSignature] = useState('');
   const [loading, setLoading] = useState(false);
+  const [messageHasFocus, setMessageHasFocus] = useState(false);
+
   const wallet = wallets.find(w => w.getID() === params.walletID);
+  const isToolbarVisibleForAndroid = Platform.OS === 'android' && messageHasFocus && isKeyboardVisible;
 
   useEffect(() => {
-    Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow', () => setIsToolbarVisibleForAndroid(true));
-    Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide', () => setIsToolbarVisibleForAndroid(false));
+    Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow', () => setIsKeyboardVisible(true));
+    Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide', () => setIsKeyboardVisible(false));
     return () => {
       Keyboard.removeListener(Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide');
       Keyboard.removeListener(Platform.OS === 'ios' ? 'keyboardWillShow' : 'keyboardDidShow');
@@ -80,90 +94,104 @@ const SignVerify = () => {
     );
 
   return (
-    <SafeBlueArea style={[styles.root, stylesHooks.root]}>
-      <BlueSpacing20 />
-      <BlueFormLabel>{loc.addresses.sign_help}</BlueFormLabel>
-      <BlueSpacing20 />
+    <SafeBlueArea>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <KeyboardAvoidingView style={[styles.root, stylesHooks.root]}>
+          {!isKeyboardVisible && (
+            <>
+              <BlueSpacing20 />
+              <BlueFormLabel>{loc.addresses.sign_help}</BlueFormLabel>
+              <BlueSpacing20 />
+            </>
+          )}
 
-      <TextInput
-        multiline
-        textAlignVertical="top"
-        blurOnSubmit
-        placeholder={loc.addresses.sign_placeholder_address}
-        placeholderTextColor="#81868e"
-        value={address}
-        onChangeText={t => setAddress(t.replace('\n', ''))}
-        testID="Signature"
-        style={[styles.text, stylesHooks.text]}
-        autoCorrect={false}
-        autoCapitalize="none"
-        spellCheck={false}
-        editable={!loading}
-      />
-      <BlueSpacing10 />
-
-      <TextInput
-        multiline
-        textAlignVertical="top"
-        blurOnSubmit
-        placeholder={loc.addresses.sign_placeholder_signature}
-        placeholderTextColor="#81868e"
-        value={signature}
-        onChangeText={t => setSignature(t.replace('\n', ''))}
-        testID="Signature"
-        style={[styles.text, stylesHooks.text]}
-        autoCorrect={false}
-        autoCapitalize="none"
-        spellCheck={false}
-        editable={!loading}
-      />
-      <BlueSpacing10 />
-
-      <TextInput
-        multiline
-        placeholder={loc.addresses.sign_placeholder_message}
-        placeholderTextColor="#81868e"
-        value={message}
-        onChangeText={setMessage}
-        testID="Message"
-        inputAccessoryViewID={BlueDoneAndDismissKeyboardInputAccessory.InputAccessoryViewID}
-        style={[styles.flex, styles.text, styles.textMessage, stylesHooks.text]}
-        autoCorrect={false}
-        autoCapitalize="none"
-        spellCheck={false}
-        editable={!loading}
-      />
-      <BlueSpacing20 />
-
-      <FContainer inline>
-        <FButton onPress={handleSign} text={loc.addresses.sign_sign} disabled={loading} />
-        <FButton onPress={handleVerify} text={loc.addresses.sign_verify} disabled={loading} />
-      </FContainer>
-      <BlueSpacing10 />
-
-      {Platform.select({
-        ios: (
-          <BlueDoneAndDismissKeyboardInputAccessory
-            onClearTapped={() => setMessage('')}
-            onPasteTapped={text => {
-              setMessage(text);
-              Keyboard.dismiss();
-            }}
+          <TextInput
+            multiline
+            textAlignVertical="top"
+            blurOnSubmit
+            placeholder={loc.addresses.sign_placeholder_address}
+            placeholderTextColor="#81868e"
+            value={address}
+            onChangeText={t => setAddress(t.replace('\n', ''))}
+            testID="Signature"
+            style={[styles.text, stylesHooks.text]}
+            autoCorrect={false}
+            autoCapitalize="none"
+            spellCheck={false}
+            editable={!loading}
           />
-        ),
-        android: isToolbarVisibleForAndroid && (
-          <BlueDoneAndDismissKeyboardInputAccessory
-            onClearTapped={() => {
-              setMessage('');
-              Keyboard.dismiss();
-            }}
-            onPasteTapped={text => {
-              setMessage(text);
-              Keyboard.dismiss();
-            }}
+          <BlueSpacing10 />
+
+          <TextInput
+            multiline
+            textAlignVertical="top"
+            blurOnSubmit
+            placeholder={loc.addresses.sign_placeholder_signature}
+            placeholderTextColor="#81868e"
+            value={signature}
+            onChangeText={t => setSignature(t.replace('\n', ''))}
+            testID="Signature"
+            style={[styles.text, stylesHooks.text]}
+            autoCorrect={false}
+            autoCapitalize="none"
+            spellCheck={false}
+            editable={!loading}
           />
-        ),
-      })}
+          <BlueSpacing10 />
+
+          <TextInput
+            multiline
+            placeholder={loc.addresses.sign_placeholder_message}
+            placeholderTextColor="#81868e"
+            value={message}
+            onChangeText={setMessage}
+            testID="Message"
+            inputAccessoryViewID={BlueDoneAndDismissKeyboardInputAccessory.InputAccessoryViewID}
+            style={[styles.flex, styles.text, styles.textMessage, stylesHooks.text]}
+            autoCorrect={false}
+            autoCapitalize="none"
+            spellCheck={false}
+            editable={!loading}
+            onFocus={() => setMessageHasFocus(true)}
+            onBlur={() => setMessageHasFocus(false)}
+          />
+          <BlueSpacing10 />
+
+          {!isKeyboardVisible && (
+            <>
+              <FContainer inline>
+                <FButton onPress={handleSign} text={loc.addresses.sign_sign} disabled={loading} />
+                <FButton onPress={handleVerify} text={loc.addresses.sign_verify} disabled={loading} />
+              </FContainer>
+              <BlueSpacing10 />
+            </>
+          )}
+
+          {Platform.select({
+            ios: (
+              <BlueDoneAndDismissKeyboardInputAccessory
+                onClearTapped={() => setMessage('')}
+                onPasteTapped={text => {
+                  setMessage(text);
+                  Keyboard.dismiss();
+                }}
+              />
+            ),
+            android: isToolbarVisibleForAndroid && (
+              <BlueDoneAndDismissKeyboardInputAccessory
+                onClearTapped={() => {
+                  setMessage('');
+                  Keyboard.dismiss();
+                }}
+                onPasteTapped={text => {
+                  setMessage(text);
+                  Keyboard.dismiss();
+                }}
+              />
+            ),
+          })}
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </SafeBlueArea>
   );
 };
@@ -175,7 +203,6 @@ export default SignVerify;
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    // paddingTop: 40,
   },
   text: {
     paddingHorizontal: 8,
@@ -188,7 +215,7 @@ const styles = StyleSheet.create({
     textAlignVertical: 'top',
   },
   textMessage: {
-    minHeight: 100,
+    minHeight: 50,
   },
   flex: {
     flex: 1,
